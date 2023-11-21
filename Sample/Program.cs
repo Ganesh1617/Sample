b1399fb3350814497f4ff664e12e0360f33960e5
Ganesh1617 <151633666+Ganesh1617@users.noreply.github.com>
2023-11-21 17:54:36 +0530

    Initial commit

---
+++ Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    internal class Program
+    {
+        public void Substraction()
+        {
+            int a = 30, b = 20, c;
+            c = a - b;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            Program g = new Program();
+            g.Substraction();
+            Console.ReadLine();
+        }
+    }
+}
